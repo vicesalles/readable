66938c7a320a28fe16cabe8c098b292d15ab86c9
Vice Sallés <vicenc.salles@gmail.com>
2017-09-28 07:42:46 +0200

    feat: AllThePosts Component

---
+++ app/src/components/Posts/AllThePosts.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+export default class AllThePosts extends Component {
+
+    example = {
+        title: "Is Bruno legit?",
+        id: "44",
+        category: "bob",
+        date: "17:14 1/10/17",
+        votes: "15"
+    }
+
+    render() {
+        return (
+            <div className="col">
+                <div className="card">
+                    <div className="card-header">
+                        <div className="row">
+                            <div className="col">
+                                <h4>All the Posts</h4>
+                            </div>
+                            <div className="col">
+                                <ul className="nav nav-pills card-header-pills">
+                                    <li className="nav-item">
+                                        <a className="nav-link active" href="#">Popular
+                                                <span className="badge badge-secondary">up</span>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Old
+                                                <span className="badge badge-secondary">up</span></a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+                    <ul className="list-group list-group-flush">
+                        <Post post={this.example}/>
+                    </ul>
+                    <div className="card-body">
+                        <div className="container">
+                            <a href="#" className="btn btn-success">Comment Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
--- app/src/layouts/Default.js
+++ app/src/layouts/Default.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import CategoriesCard from '../components/categories/CategoriesCard';
 import TopPosts from '../components/Posts/TopPosts';
+import AllThePosts from '../components/Posts/AllThePosts';
 
 
 export default class Default extends Component {
@@ -12,6 +13,12 @@ export default class Default extends Component {
                 <div className="row">
                     <CategoriesCard />
                     <TopPosts />
+                </div>
+                <div className="container">
+                    <div className="cont">&nbsp;</div>
+                </div>
+                <div className="row">
+                    <AllThePosts />
                 </div>
             </div>
         );
