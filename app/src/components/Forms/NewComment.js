194d816490657ec7ee0346f0ffacbcbac4cdef30
Vice Sallés <vicenc.salles@gmail.com>
2017-10-24 07:36:31 +0200

    fix: Bootstrap style issues

--- app/src/components/Forms/NewComment.js
+++ app/src/components/Forms/NewComment.js
@@ -7,60 +7,58 @@ import * as utils from '../../utils/helpers';
 class NewComment extends Component {
     state = {
         comment: '',
-        owner:''
+        owner: ''
     }
     componentDidMount() {
         this.refs.comentari.focus();
     }
-    handleChange = (e,t) => {
+    handleChange = (e, t) => {
         this.setState({ [t]: e.target.value });
     }
     handleSubmit = (e) => {
         e.preventDefault();
 
-        //Create comment object
+        //Creating the comment object
         let q = {}
-        q.timestamp= Date.now();
-        q.id= utils.guid();
-        q.body=this.state.comment;
-        q.owner= this.state.owner;
+        q.timestamp = Date.now();
+        q.id = utils.guid();
+        q.body = this.state.comment;
+        q.owner = this.state.owner;
         q.parentId = this.props.match.params.id;
-
-        /*
-                id: Any unique ID. As with posts, UUID is probably the best here.  
-            timestamp: timestamp. Get this however you want.  
-            body: String  
-            owner: String  
-            parentId: Should match a post id in the database.  
-        */
+       
         this.props.dispatch(addComment(q));
 
     }
     render() {
         return (
-            <div className="row row-space" >
-                <div className="col">
-                    <div className="card">
 
-                        <div className="card-header">
-                            Comment
+            <div className="col-12">
+                <div className="card">
+
+                    <div className="card-header">
+                        Comment
                     </div>
-                        <div className="card-block">
-                            <div className="container">
-                                <form>
-                                    <textarea onChange={(e) => { this.handleChange(e,'comment') }} required ref="comentari" value={this.state.comment} className="form-control" cols="30"></textarea>
-                                    <input type="text" onChange={(e)=>{this.handleChange(e,'owner')}} value={this.state.owner} placeholder="Your name"/>
-                                    <button onClick={(e) => { this.handleSubmit(e) }} type="submit" className="btn btn-primary">Submit</button>
-                                </form>
-                            </div>
+                    <div className="card-block">
+                        
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group">
+                                    <textarea onChange={(e) => { this.handleChange(e, 'comment') }} required ref="comentari" value={this.state.comment} className="form-control" cols="30"></textarea>
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" onChange={(e) => { this.handleChange(e, 'owner') }} value={this.state.owner} placeholder="Your name" />
+                                  </div>
+                                  <button onClick={(e) => { this.handleSubmit(e) }} type="submit" className="btn btn-primary btn-sm clicable">Submit</button>
+                               
+                            </form>
                         </div>
-
-
-
                     </div>
-                </div>
 
+
+
+                </div>
             </div>
+
         )
     }
 }
