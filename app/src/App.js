92f11e98a06cfa6cbf114a1aebb964d5374be749
Vice Sallés <vicenc.salles@gmail.com>
2017-10-30 15:24:53 +0100

    refactor: turn component into stateless function

--- app/src/App.js
+++ app/src/App.js
@@ -7,27 +7,24 @@ import Post from './layouts/Post';
 import Edit from './layouts/Edit';
 import Broken from './layouts/Broken';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-       
-        <Switch>
-          <Route path="/" exact component={Default} />
-          <Route path="/category/:id" component={Category} />
-          <Route path="/post/:id" component={Post} />
-          <Route path="/edit/:id" component={Edit} />
-          <Route component={Broken} />
-        </Switch>
-       
-        <div className="container">
-          <div className="cont">&nbsp;</div>
-        </div>
+function App() {
+
+  return (
+    <div className="container-fluid">
+
+      <Switch>
+        <Route path="/" exact component={Default} />
+        <Route path="/category/:id" component={Category} />
+        <Route path="/post/:id" component={Post} />
+        <Route path="/edit/:id" component={Edit} />
+        <Route component={Broken} />
+      </Switch>
+
+      <div className="container">
+        <div className="cont">&nbsp;</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-
 
 export default withRouter(connect()(App));
